d298e6f35c068e87e2d04a36da45e22749a6e6df
Masum Billah <masumbillah6584@gmail.com>
2022-11-08 17:22:55 +0600

    send data for homepage and all food

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
+
+//midlewere
 app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 8000;
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_pASSWORD}@cluster0.pwgovse.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const dbHandler = async () => {
+  try {
+    const foodCollection = client.db("tastyBite").collection("Food");
+    app.get("/homefood", async (req, res) => {
+      const query = {};
+      const results = await foodCollection.find(query).limit(3).toArray();
+      res.send(results);
+    });
+    app.get("/allfood", async (req, res) => {
+      const query = {};
+      const results = await foodCollection.find(query).toArray();
+      res.send(results);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+dbHandler().catch((err) => console.log(err));
 app.get("/", (req, res) => {
   res.send("Server Is Running");
 });
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
+  console.log(uri);
 });
---
+++ link.js
@@ -0,0 +1,12 @@
+// https://i.postimg.cc/76BjZKpG/banner-img.jpg
+// https://i.postimg.cc/GmCRN5rd/banner-img1.jpg
+// https://i.postimg.cc/m2MRmGvm/banner-img2.jpg
+
+// https://i.postimg.cc/zv6MnVDq/beef.jpg
+// https://i.postimg.cc/Hs8FT43n/breakfast.jpg
+// https://i.postimg.cc/85kqG2ng/dessert.jpg
+// https://i.postimg.cc/4ymq7zg8/dinner.jpg
+// https://i.postimg.cc/xj4WrFvB/fish.jpg
+// https://i.postimg.cc/k5MmMRBH/lunch.jpg
+// https://i.postimg.cc/j2fV3zFD/veg.jpg
+// https://i.postimg.cc/sfM3ZWwd/logo.jpg
